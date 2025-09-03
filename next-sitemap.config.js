0713d2f207327d19854ce80d75480ae1b9ea866c
lenin12345-dev <leninmohapatra@gmail.com>
2025-09-03 00:35:34 -0400

    add public folder

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   siteUrl: 'https://generalrooterxp.netlify.app',
   generateRobotsTxt: true,
-  sitemapBaseFileName: 'sitemap_index',
+  sitemapBaseFileName: 'sitemap_index',  // generates sitemap_index.xml
   sitemapSize: 5000,
   changefreq: 'weekly',
   priority: 0.7,
   generateIndexSitemap: true,
-  autoLastmod: true,          
+  autoLastmod: true,
+  outDir: './public', // ✅ ensures sitemaps are placed in public folder
 };
-
